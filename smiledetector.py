497e7d4ec718a86dfb3b9cba677032e6ed62a8db
emirhanylmzz <46828131+emirhanylmzz@users.noreply.github.com>
2020-01-16 19:47:43 +0300

    Add files via upload

---
+++ smiledetector.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 16:29:12 2019
+Emirhan YILMAZ CV & AI HOMEWORK (SMİLE DETECTOR)
+@author: Acer
+"""
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+upperbody_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
+lowerbody_cascade = cv2.CascadeClassifier('haarcascade_lowerbody.xml')
+
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for(x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 22)
+        for(ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+    upper = upperbody_cascade.detectMultiScale(gray, 1.1, 7,  minSize = (40, 70))
+    for(x, y, w, h) in upper:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 2)
+    lower = lowerbody_cascade.detectMultiScale(gray, 1.1, 5,  minSize = (50, 50))
+    for(x, y, w, h) in lower:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 2)
+    return frame
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+    #frame = cv2.resize(frame, None, fx=1, fy=1 , interpolation=cv2.INTER_AREA)
+    if ret:    
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        canvas = detect(gray, frame)
+        cv2.imshow('Video', canvas) #Display the outputs.
+        if cv2.waitKey(1) & 0xFF == ord('q'): # If we type on the keyboard:
+            break # Stop the loop.
+    else:
+        print("Couldn't")
+video_capture.release() # Turning the webcam off.
+cv2.destroyAllWindows() 
